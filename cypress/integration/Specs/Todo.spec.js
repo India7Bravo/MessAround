2e2910c70fc10b12eaef2860c7aaa5d4d716725e
India7Bravo <india7bravo@gmail.com>
2022-05-21 11:48:37 +0200

    Changed the spec file name and added in more assertions for the tests
    Changed the spec file name and added in more assertions for the tests

---
+++ cypress/integration/Specs/Todo.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('It visits the todo website', () => {
+    before(() => {
+        cy.goTo()
+    })
+
+    // Creates the list items
+    it('Creates the first list item', () => {
+        cy.get('.new-todo').type('Fkn owning this sh*t{enter}')
+        cy.get('.new-todo').type('Attempting to own this shit{enter}')
+        cy.get('.new-todo').type('Another test to try out{enter}')
+        cy.get('.todo-list li').should('have.length', 3)
+    })
+
+    // Creates the third list item and confirms the amount of list items
+    it('Creates the third list item and confirms the amount of list items', () => {
+        // Confirms there's 3 list items and the li titles
+        cy.get('.todo-list li').should('have.length', 3)
+
+        // Confirms the list item names
+        cy.get('.todo-list li').eq(0).should('have.text', 'Fkn owning this sh*t')
+        cy.get('.todo-list li').eq(1).should('have.text', 'Attempting to own this shit')
+        cy.get('.todo-list li').eq(2).should('have.text', 'Another test to try out')
+    })
+
+    // Checks off a list item and ensures it has been completed 
+    it("Checks off a list item and ensures it has been completed", () => {
+        // Checks the list item "Fkn owning this sh*t"
+        cy.contains('Fkn owning this sh*t')
+        .parent()
+        .find('input[type=checkbox]')
+        .check()
+
+        // Ensures the list item "Fkn owning this sh*t" is completed
+        cy.contains('Fkn owning this sh*t')
+        .parents('li')
+        .should('have.class', 'completed')
+    })
+})
